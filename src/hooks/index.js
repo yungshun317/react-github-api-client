47689a8075cd3080dc148a592177c14b1e760605
yungshun317 <seraph317@gmail.com>
2021-06-05 20:52:39 +0800

    [FEAT] Create the `useIterator` hook that iterates through any array of objects

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -24,3 +24,23 @@ export function useFetch(uri) {
 		error
 	};
 }
+
+// [7] Iterate through any array of objects
+export const useIterator = (
+	items = [],
+	initialIndex = 0
+) => {
+    const [i, setIndex] = useState(initialIndex);
+    
+    const prev = () => {
+    	if (i === 0) return setIndex(items.length - 1);
+    	setIndex(i - 1);
+    };
+
+    const next = () => {
+    	if (i === items.length - 1) return setIndex(0);
+    	setIndex(i + 1);
+    }
+
+    return [items[i], prev, next];
+}
