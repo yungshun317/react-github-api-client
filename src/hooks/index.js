89a2a7956552aee28f55dc443c852ea0bf411070
yungshun317 <seraph317@gmail.com>
2021-06-05 17:43:48 +0800

    [FEAT] Create the custom `useFetch` hook for reusability

---
+++ src/hooks/index.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+// [4] Create the custom hook for reusability
+export function useFetch(uri) {
+    // Success
+	const [data, setData] = useState();
+	// Error
+	const [error, setError] = useState();
+	// Pending
+	const [loading, setLoading] = useState();
+
+	useEffect(() => {
+		if (!uri) return;
+		fetch(uri)
+		    .then(data => data.json())
+		    .then(setData)
+		    .then(() => setLoading(false))
+		    .catch(setError);
+	}, [uri]);
+
+	return {
+		loading,
+		data,
+		error
+	};
+}
