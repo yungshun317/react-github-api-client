c1ae397eeac8c4da77b218352e27a2c305c41d03
yungshun317 <seraph317@gmail.com>
2021-06-06 16:34:52 +0800

    [DOCS] Add a `README.md` file

--- src/RepositoryReadme.js
+++ src/RepositoryReadme.js
@@ -2,23 +2,23 @@ import React, { useState, useCallback, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import "regenerator-runtime/runtime";
 
-// [10] 
+// [10] Request waterfall requests and render Markdown
 export default function RepositoryReadme({ repo, login }) {
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState();
 	const [markdown, setMarkdown] = useState("");
 
-	const loadReadme = async (login, repo) => {
+	const loadReadme = useCallback(async (login, repo) => {
 		setLoading(true);
 		const uri = `https://api.github.com/repos/${login}/${repo}/readme`;
 		const { download_url } = await fetch(uri).then(res => res.json());
         const markdown = await fetch(download_url).then(res => res.text());
 		setMarkdown(markdown);
 		setLoading(false);
-	};
+	}, []);
 
 	useEffect(() => {
-		//if (!repo || !login) return;
+		if (!repo || !login) return;
 		loadReadme(login, repo).catch(setError);
 	}, [repo]);
 
